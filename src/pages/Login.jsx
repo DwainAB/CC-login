6cd12f75afabce41f8f170e833c98578292ff9b7
dwain93 <74243765+dwain93@users.noreply.github.com>
2025-05-14 13:40:46 +0200

    Ajout du login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
-import { Form, Button, Container, Card, Row, Col } from "react-bootstrap";
+import { Form, Button, Container, Card, Row, Col, Alert } from "react-bootstrap";
+import { useNavigate } from "react-router";
 
 const LoginPage = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+  const navigate = useNavigate()
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -14,14 +17,42 @@ const LoginPage = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit =  async (e) => {
     e.preventDefault();
-    // Handle login logic here
-    // Don't forget to handle errors, both for yourself (dev) and for the client (via a Bootstrap Alert):
-    //   - Show an error if credentials are invalid
-    //   - Show a generic error for all other cases
-    // On success, redirect to the Pro Offers page
-    console.log("Login submitted:", formData);
+    try{
+
+      const response = await fetch('https://offers-api.digistos.com/api/auth/login',{
+        method: "POST",
+        headers:{
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+        },
+      )
+
+
+      if (!response.ok) {
+        const data = await response.json();
+        const error = new Error(
+           data.error || "Une erreur est survenue lors de l'inscription."
+        );
+        error.status = response.status;
+        throw error;
+     }
+
+      navigate('/offres/professionnelles')
+
+    }catch(error){
+      console.error('Une erreur est survenue', error)
+
+      if(error.status === 401){
+        setErrorMessage('Email ou mot de passe incorrect')
+      }else{
+        setErrorMessage('Une erreur est survenue')
+      }
+
+    } 
   };
 
   return (
@@ -30,6 +61,14 @@ const LoginPage = () => {
         <Col xs={12} sm={8} md={6} lg={4}>
           <Card className="p-4 shadow-lg">
             <h2 className="text-center mb-4">Se connecter</h2>
+
+            {errorMessage && (
+              <Alert variant="danger" dismissible>
+                <Alert.Heading>Erreur!</Alert.Heading>
+                <p>{errorMessage}</p>
+              </Alert>
+            )}
+
             <Form onSubmit={handleSubmit}>
               <Form.Group className="mb-3" controlId="loginEmail">
                 <Form.Label>Email</Form.Label>
